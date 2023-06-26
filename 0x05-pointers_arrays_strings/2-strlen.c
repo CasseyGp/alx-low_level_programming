73499b853d1fe0d6c54619e8774fe6b9f426b6a7
CasseyGp <nwaforgodspowercassey@gmail.com>
2023-06-26 14:42:08 -0700

    commiting for strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include <string.h>
+
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s: returning string length
+ *
+ * Return: string length
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
